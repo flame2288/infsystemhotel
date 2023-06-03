26dcc959588bbda3d56dd7bef0ceb5da91b41faa
flame2288 <133582517+flame2288@users.noreply.github.com>
2023-06-03 11:08:47 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -11,91 +11,20 @@ require "dbconnect.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Document</title>
-    <style>
-        body{
-            max-width: 1100px;
-        }
-        *{
-            font-family: Arial;
-            margin: 0 auto;
-        }
-        .item{
-
-            background-color: palegreen;
-            margin: 20px;
-            padding: 10px;
-            border-radius: 10px;
-
-
-        }
-        .name{
-            vertical-align: top;
-            width: 20%;
-            max-width: 250px;
-            display: inline-block;
-        }
-        .props{
-            vertical-align: top;
-            width: 57%;
-            display: inline-block;
-        }
-        .price{
-            vertical-align: top;
-            width: 10%;
-            max-width: 150px;
-            display: inline-block;
-            font-weight: bold ;
-            text-align: right;
-            font-size: large;
-            margin-right: 15px;
-        }
-        .image{
-            width: 10%;
-            display: inline-block;
-        }
-        .bin-button{
-            align: right;
-            padding: 10px;
-            margin: 5px;
-            border-radius: 8px;
-            border-width: 1px;
-            border-color: green;
-            width: 120px;
-        }
-
-    </style>
 </head>
 <body>
-<h1>информационная система гостиничного комплекса</h1>
+<h1>infsyshotel</h1>
 
 <?php
-echo "<h2>информационная система гостиничного комплекса</h2>";
+echo "<h2>infsyshotel</h2>";
 
-$result = $conn->query("SELECT * FROM items");
+$result = $conn->query("SELECT * FROM гости");
 
 while ($row = $result->fetch()){
-    ?>
-    <div class="item">
-        <div class="image">
-            <img src="images/item.png" width="60px">
-        </div>
-        <div class="name">
-            <?=$row['name']?>
-        </div>
-        <div class="props">
-            <?=$row['properties']?>
-        </div>
-        <div class="price">
-            <?=$row['price']?>
-            <button class="bin-button"><i class="fa fa-cart-plus"></i>&nbsp&nbsp В корзину</button>
-        </div>
-    </div>
-    <?php
-
+    echo $row['ID'];
+    echo $row['Name'];
 }
 ?>
 
-
 </body>
 </html>
-
